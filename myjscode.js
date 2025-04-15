895fbbbd984926c757fa82d62f36987b90a7f3dd
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-04-15 11:48:14 +0530

    prototype keyword

--- myjscode.js
+++ myjscode.js
@@ -17,3 +17,40 @@ console.log(personData.personName + " has these cars:");
 console.log(personData.personCars["car1"]);
 console.log(personData.personCars["car2"]);
 console.log(personData.personCars["car3"]);
+
+console.log("-----------------------------------------------")
+function Product(productid,productname,price,categoryid)
+{
+this.prodid=productid;
+this.prodname=productname;
+this.productprice=price;
+this.catid=categoryid;
+
+this.productDetails=function()
+{
+return this.prodid + " " + this.prodname;
+
+};
+
+
+
+}
+
+const productobj=new Product(1,"Tea",10,101);
+console.log(productobj.prodid);
+console.log(productobj.prodname);
+console.log(productobj.productprice);
+console.log(productobj.catid);
+
+document.getElementById("details").innerHTML=productobj.productDetails();
+
+Product.prototype.changeProductName=function(name){
+this.prodname=name;
+
+}
+productobj.changeProductName("coffee");
+console.log("After changing product data details: ")
+console.log(productobj.prodid);
+console.log(productobj.prodname);
+console.log(productobj.productprice);
+console.log(productobj.catid);
